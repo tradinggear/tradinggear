bb6e62a3e663d158d6e0623a6b7734e310d10ab6
ain.jo.dev@gmail.com <ainjo1117@ciel.co.kr>
2025-09-02 16:11:40 +0900

    빌드 오류 문제 수정

--- app/routes/main.tsx
+++ app/routes/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+/*import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
@@ -6,4 +6,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+)*/
+import App from "./App";
+
+export default function MainRoute() {
+  // 여기서는 절대 document/window/ReactDOM 사용 X
+  return <App />;
+}
